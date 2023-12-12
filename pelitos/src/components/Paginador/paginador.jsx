a36b0a02947ba6459a6663688905715f1880cffc
Isabel-Martha <marthaisabelcallejo@gmail.com>
2023-12-11 21:52:37 -0300

    mas cambios

---
+++ pelitos/src/components/Paginador/paginador.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+const Paginador = ({ currentPage, totalPages, onPageChange }) => {
+  const [inputPage, setInputPage] = useState(currentPage);
+
+  const handleInputChange = (e) => {
+    const value = parseInt(e.target.value);
+    setInputPage(value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      onPageChange(inputPage);
+    }
+  };
+
+  return (
+    <div>
+      <button disabled={currentPage === 1} onClick={() => onPageChange(1)}>
+        Primera
+      </button>
+      <button disabled={currentPage === 1} onClick={() => onPageChange(currentPage - 1)}>
+        Anterior
+      </button>
+      <span>
+        Página{' '}
+        <input
+          type="number"
+          value={inputPage}
+          onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
+        />{' '}
+        de {totalPages}
+      </span>
+      <button disabled={currentPage === totalPages} onClick={() => onPageChange(currentPage + 1)}>
+        Siguiente
+      </button>
+      <button disabled={currentPage === totalPages} onClick={() => onPageChange(totalPages)}>
+        Última
+      </button>
+    </div>
+  );
+};
+
+export default Paginador;
--- pelitos/src/components/Productos/productos.jsx
+++ pelitos/src/components/Productos/productos.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import './productos.css';
+import Paginador from '../Paginador/paginador';
+import Buscador from '../Buscador/buscador';
+import ProductosTabla from '../ProductosTabla/productostabla';
 const CrearProducto = () => {
+    const [showForm, setShowForm] = useState(false);
     const [nuevoProducto, setNuevoProducto] = useState({
         nombre: '',
         descripcion: '',
         precio: 0,
-        imagenes: null, // El campo de archivos debe ser inicializado como null
+        imagenes: null,
     });
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [productos, setProductos] = useState([]); // Definir el estado de los productos
 
     const navigate = useNavigate();
 
@@ -15,9 +22,35 @@ const CrearProducto = () => {
         const { name, value, files } = e.target;
         setNuevoProducto((prevProducto) => ({
             ...prevProducto,
-            [name]: files ? files[0] : value, // Si es un campo de archivo, actualiza con el archivo
+            [name]: files ? files[0] : value,
         }));
     };
+    const handlePagination = async (page) => {
+        try {
+            const response = await fetch(`https://localhost:7074/Productos?page=${page}&size=10`);
+            if (!response.ok) {
+                throw new Error(`Error al obtener productos. Status: ${response.status}`);
+            }
+            const data = await response.json();
+            setProductos(data);
+            setCurrentPage(page);
+        } catch (error) {
+            console.error('Error al obtener los productos:', error);
+        }
+    };
+    const handleSearch = async (searchTerm) => {
+        try {
+            const response = await fetch(`https://localhost:7074/Productos?search=${searchTerm}`);
+            if (!response.ok) {
+                throw new Error(`Error al obtener productos. Status: ${response.status}`);
+            }
+            const data = await response.json();
+            setProductos(data); // Actualiza el estado de los productos con los resultados de la búsqueda
+        } catch (error) {
+            console.error('Error al buscar productos:', error);
+        }
+    };
+    
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -26,9 +59,10 @@ const CrearProducto = () => {
             formData.append('title', nuevoProducto.nombre);
             formData.append('description', nuevoProducto.descripcion);
             formData.append('price', nuevoProducto.precio);
-            formData.append('image', nuevoProducto.imagenes);
+            formData.append('file', nuevoProducto.imagenes);
+            formData.append('idTipoProducto', 4);
 
-            const response = await fetch('https://localhost:7074/Producto/Type', {
+            const response = await fetch('https://localhost:7074/Producto', {
                 method: 'POST',
                 body: formData,
             });
@@ -37,31 +71,62 @@ const CrearProducto = () => {
                 throw new Error(`Error al crear el producto. Status: ${response.status}`);
             }
 
-            navigate('/productos'); // Redireccionar después de crear el producto
+            // Limpiar los campos después de un POST exitoso
+            setNuevoProducto({
+                nombre: '',
+                descripcion: '',
+                precio: 0,
+                imagenes: null,
+            });
+
+            navigate('/productos');
+            handlePagination(currentPage);
         } catch (error) {
             console.error('Error al crear el producto:', error);
         }
+          
+    };
+
+    const toggleForm = () => {
+        setShowForm(!showForm);
     };
 
     return (
         <div>
-            <h2>Crear Producto</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Nombre:
-                    <input type="text" name="nombre" onChange={handleChange} />
-                </label>
-                <label>Descripción:
-                    <textarea name="descripcion" onChange={handleChange} />
-                </label>
-                <label>Precio:
-                    <input type="number" name="precio" onChange={handleChange} />
-                </label>
-                <label>Imágenes:
-                    <input type="file" name="imagenes" onChange={handleChange} />
-                </label>
-                <button type="submit">Crear Producto</button>
-            </form>
+            <button onClick={toggleForm} className='primary'>Crear Producto</button>
+            {showForm && (
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <label>Nombre:
+                            <input type="text" name="nombre" onChange={handleChange} />
+                        </label>
+                        <label>Descripción:
+                            <textarea name="descripcion" onChange={handleChange} />
+                        </label>
+                        <label>Precio:
+                            <input type="number" name="precio" onChange={handleChange} />
+                        </label>
+                        <label>Imágenes:
+                            <input type="file" name="imagenes" onChange={handleChange} />
+                        </label>
+                        <button type="submit" className='primary'>Crear Producto</button>
+                        <button onClick={toggleForm} className='danger'>Cancelar</button>
+                    </form>
+                    
+                </div>
+                
+            )}
+             <ProductosTabla productos={productos} />
+            <Buscador onSearch={handleSearch} />
+            <Paginador
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={handlePagination}
+/>
+
         </div>
+        
+        
     );
 };
 
