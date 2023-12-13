203ee0323d07ea5d73fde4e93e5c5644df3e589e
Isabel-Martha <marthaisabelcallejo@gmail.com>
2023-12-13 15:51:12 -0300

    El Final Tan Esperado :)

--- pelitos/src/components/Productos/productos.jsx
+++ pelitos/src/components/Productos/productos.jsx
@@ -1,49 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Table from 'react-bootstrap/Table';
 import { Link } from 'react-router-dom';
 import { ImSpinner3 } from 'react-icons/im';
 import AgregarProducto from '../AgregarProducto/agregarproducto';
-
-
+import Paginador from '../Paginador/paginador';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 import './productos.css';
-import { useEffect } from 'react';
-import { useState } from 'react';
 
 export const Productos = () => {
-    const [showAgregar, setShowAgregar] = useState(false);// Definición de la variable showAgregar y setShowAgregar
-    const fetchData = async () => {
-        try {
-            let response = await fetch("https://localhost:7074/Producto/Type/1");
-            let json = await response.json();
-            
-            setProductos(json);
+    const [showAgregar, setShowAgregar] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [productos, setProductos] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(10);
+    const [searchTerm, setSearchTerm] = useState('');
 
-            console.log(json);
-        } catch(e) {
-    
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const fetchData = useCallback(async () => {
+        try {
+            let url = `https://localhost:7074/Producto/Type/1?page=${currentPage}&limit=${itemsPerPage}`;
+            if (searchTerm) {
+                url += `&query=${searchTerm}`;
+            }
+            let response = await fetch(url);
+            let json = await response.json();
+            setProductos(json.list);
+        } catch (error) {
+            // Manejo de errores
         } finally {
             setLoading(false);
         }
-    }
-    
-    const [loading, setLoading] = useState(true);
-    const [productos, setProductos] = useState([]);
+    }, [currentPage, itemsPerPage, searchTerm]);
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
     useEffect(() => {
         setLoading(true);
-        fetchData();  
-    }, []);
-    
+        fetchData();
+    }, [currentPage, fetchData, searchTerm]);
+
     return (
-        
         <>
-        <button className="btn btn-primary" onClick={() => setShowAgregar(true)}>Agregar Producto</button>
-        {showAgregar && <AgregarProducto setShowAgregar={setShowAgregar} />}
-            {
-                loading ?
-                    <div className='spinner'><ImSpinner3/></div> :
+            <button className="btn btn-primary" onClick={() => setShowAgregar(true)}>Agregar Producto</button>
+            {showAgregar && <AgregarProducto setShowAgregar={setShowAgregar} />}
+            {loading ?
+                <div className='spinner'><ImSpinner3 /></div> :
+                <>
                     <Table striped bordered hover>
                         <thead>
                             <tr>
@@ -54,9 +62,8 @@ export const Productos = () => {
                             </tr>
                         </thead>
                         <tbody>
-                        {
-                            productos.map((producto) => {
-                                return (<tr>
+                            {productos.map((producto) => (
+                                <tr key={producto.id}>
                                     <td>{producto.description}</td>
                                     <td>{producto.id}</td>
                                     <td>{producto.title}</td>
@@ -65,13 +72,25 @@ export const Productos = () => {
                                         <Link to="" className="btn btn-primary">Editar</Link>
                                         <Link to="" className="btn btn-secondary">Eliminar</Link>
                                     </td>
-                                </tr>);
-                            })
-                        }
+                                </tr>
+                            ))}
                         </tbody>
                     </Table>
-}
-            
+                    <input
+                        type="text"
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                        placeholder="Buscar productos..."
+                    />
+                    <Paginador
+                        currentPage={currentPage}
+                        totalPages={Math.ceil(productos.length / itemsPerPage)}
+                        onPageChange={handlePageChange}
+                    />
+                </>
+            }
         </>
     );
-}
+};
+
+export default Productos;
